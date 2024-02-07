2d78714d8e7e4b3aeba572d84369028333644b25
LudoPilot <e.berardfr@gmail.com>
2024-02-07 09:59:51 +0100

    Ajout des méthodes CRUD dans ArticleController. À tester plus tard.

--- back-end/app/Http/Controllers/ArticleController.php
+++ back-end/app/Http/Controllers/ArticleController.php
@@ -2,9 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
-    //
+    public function index()
+	{
+		$articles = Article::all();
+		return response()->json($articles);
+	}
+
+	public function show($id)
+	{
+		$article = Article::create();
+		return response()->json($article);
+	}
+
+	public function store(Request $request)
+	{
+		$article = Article::create($request->all());
+		return response()->json($article, 201);
+	}
+	
+    public function update(Request $request, $id)
+    {
+        $article = Article::find($id);
+        $article->update($request->all());
+        return response()->json($article, 200);
+    }
+
+	public function destroy($id)
+	{
+		Article::destroy($id);
+		return response()->json(null, 204);
+	}
 }
