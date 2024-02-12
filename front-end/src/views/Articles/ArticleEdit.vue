a5b039089e612133508f01907d310e5cadea3153
LudoPilot <e.berardfr@gmail.com>
2024-02-12 16:08:08 +0100

    Création du template pour ArticleEdit

--- front-end/src/views/Articles/ArticleEdit.vue
+++ front-end/src/views/Articles/ArticleEdit.vue
@@ -1,1 +1,36 @@
-<template></template>
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import type { ArticleFormData } from '@/types/ArticleFormData';
+// ajouter Axios plus tard
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+const articleId = route.params.id;
+
+const articleForm = ref<ArticleFormData>({
+	title: '',
+	content: '',
+	category: ''
+});
+</script>
+
+<template>
+	<div>
+		<h1>Modifier l'Article</h1>
+		<form @submit.prevent="submitForm">
+			<div>
+				<label for="title">Titre</label>
+				<input v-model="articleForm.title" id="title" required>
+			</div>
+			<div>
+				<label for="content">Contenu</label>
+				<textarea v-model="articleForm.content" id="content" required></textarea>
+			</div>
+			<div>
+				<label for="category">Catégorie</label>
+				<input v-model="articleForm.category" id="category">
+			</div>
+			<button type="submit">Mettre à jour</button>
+		</form>
+	</div>
+</template>
